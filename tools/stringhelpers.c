efca6ff8157b7c1ec251aa9da9fd7e65d20a81bf
Alexander Grothe <seahawk1986@gmx.de>
2021-04-01 14:23:05 +0200

    Add cJsonLoader for easier Access to json data

--- tools/stringhelpers.c
+++ tools/stringhelpers.c
@@ -1,10 +1,157 @@
-
+#include <sstream>
 #include "stringhelpers.h"
+#include "../lib/common.h"
+
+cJsonNode::cJsonNode(const char* jsonString) {
+    json_error_t err;
+    obj = json_loads(jsonString, 0, &err);
+    if (!obj) {
+        tell(0, "could not parse json, error on line %d, column %d", err.line, err.column);
+    }
+    root = std::shared_ptr<json_t>(obj, [](json_t* data){json_decref(data); data=nullptr;});
+}
+
+cJsonNode::cJsonNode(const std::string &jsonString) {
+    json_error_t err;
+    obj =  json_loads(jsonString.c_str(), 0, &err);
+        if (!obj) {
+        tell(0, "could not parse json, error on line %d, column %d", err.line, err.column);
+    }
+    root = std::shared_ptr<json_t>(obj, [](json_t* data){json_decref(data);});
+}
+
+cJsonNode::cJsonNode(json_t* data): obj(data), root(nullptr) {}
+
+cJsonNode::cJsonNode(json_t* data, std::shared_ptr<json_t> root): obj(data), root(root) {}
+
+bool cJsonNode::isNull(void)     { return !obj || json_is_null(obj);    }  // we need to check if this is a nullptr or a json NULL object
+bool cJsonNode::isObject(void)   { return  obj && json_is_object(obj);  }
+bool cJsonNode::isInt(void)      { return  obj && json_is_integer(obj); }
+bool cJsonNode::isNumber(void)   { return  obj && json_is_number(obj);  }
+bool cJsonNode::isString(void)   { return  obj && json_is_string(obj);  }
+bool cJsonNode::isDouble(void)   { return  obj && json_is_real(obj);    }
+bool cJsonNode::isArray(void)    { return  obj && json_is_array(obj);   }
+size_t cJsonNode::currentArrayIndex(void) { return idx; }
+size_t cJsonNode::arraySize(void){ return json_array_size(obj); }
+void cJsonNode::resetArrayIterator(void) { idx = 0; }
+
+
+cJsonNode cJsonNode::nextElementInArray() {
+    if (isArray() && idx + 1 < json_array_size(obj)) {
+        return json_array_get(obj, idx++);
+    }
+    idx = 0;
+    return cJsonNode();
+}
+
+cJsonNode cJsonNode::operator[](const size_t index) {
+    return json_array_get(obj, index);
+}
+
+cJsonNode cJsonNode::operator[](const char* name) {
+    return json_object_get(obj, name);
+}
+
+cJsonNode cJsonNode::operator[](const char* name) const {
+    return json_object_get(obj, name);
+}
+
+cJsonNode cJsonNode::operator[](const std::string &name) {
+    return json_object_get(obj, name.c_str());
+}
+
+cJsonNode cJsonNode::operator[](const std::string &name) const {
+    return json_object_get(obj, name.c_str());
+}
+
+
+std::vector<cJsonNode> cJsonNode::ArrayToVector(void) {
+    std::vector<cJsonNode> result;
+    result.reserve(json_array_size(obj));
+    size_t idx;
+    json_t *element;
+    json_array_foreach(obj, idx, element) {
+        result.emplace_back(element);
+    }
+    return result;
+}
+
+cJsonNode cJsonNode::getElementInArray(const size_t index) {
+         if (index < json_array_size(obj)) return json_array_get(obj, index);
+         return cJsonNode();
+}
+
+json_t* cJsonNode::object(void)  { return obj; }
+
+
+json_t* cJsonNode::objectByName(const char* name) {
+    return json_object_get(obj, name);
+}
+
+cJsonNode cJsonNode::nodeByName(const char* name) {
+    return cJsonNode(objectByName(name), root);
+}
+
+int cJsonNode::intByName(const char* name) {
+    json_t* int_json = json_object_get(obj, name);
+    int result = json_integer_value(int_json);
+    return result;
+}
+
+std::string cJsonNode::stringByName(const char* name) {
+    json_t* str_json = objectByName(name);
+    auto content = json_string_value(str_json);
+    std::string result{content ? content : std::string()};
+    return result;
+}
+double cJsonNode::doubleByName(const char* name) {
+    json_t* double_json = objectByName(name);
+    double result = json_real_value(double_json);
+    return result;
+}
+
+double cJsonNode::doubleValue() { return json_number_value(obj);}
+
+int cJsonNode::intValue() { return json_integer_value(obj); }
+
+std::string cJsonNode::stringValue() {
+         auto content = json_string_value(obj);
+         return content ? content : std::string();
+}
+
+std::string cJsonNode::combine_jsonArrayStrings(const char* sep) {
+    ostringstream result;
+    size_t array_size = json_array_size(obj);
+    size_t max_idx = array_size - 1;
+    if (json_is_array(obj) && array_size > 0) {
+        result << sep;
+        size_t idx;
+        json_t* element;
+        json_array_foreach(obj, idx, element) {
+            auto content = json_string_value(element);
+            if (content) {
+                result << content;
+                if (idx < max_idx) result << sep;
+            }
+        }
+    }
+    return result.str();
+}
+
+void cJsonNode::addStringArrayToVector(std::vector<std::string> &result) {
+    size_t n_elements = json_array_size(obj);
+    result.reserve(n_elements + result.size());
+    for (idx = 0; idx + 1 < n_elements;) {
+        result.push_back(nextElementInArray().stringValue());
+    }
+}
+
+
 
 cJsonLoader::cJsonLoader(const char* name)
 {
    err = new json_error_t;
-   obj = json_loads(name, 0, err); 
+   obj = json_loads(name, 0, err); // TODO: why is there no check for errors? Should this fail silently?
 
 }
 
@@ -26,6 +173,70 @@ int cJsonLoader::isObject()
 { 
    return obj && json_is_object(obj); 
 }
+
+// TODO: remove this block
+// std::string extractJsonString(const json_t *object, const char *key) {
+//         json_t* str_json = json_object_get(object, key);
+//         auto content = json_string_value(str_json);
+//         std::string result{content ? content : std::string()};
+//         return result;
+// }
+
+// int extractJsonInt(const json_t *object, const char *key) {
+//         json_t* int_json = json_object_get(object, key);
+//         int result = json_integer_value(int_json);
+//         return result;
+// }
+
+// double extractJsonDouble(const json_t *object, const char *key) {
+//         json_t* double_json = json_object_get(object, key);
+//         double result = json_real_value(double_json);
+//         return result;
+// }
+
+// std::vector<std::string> get_jsonArrayStrings(const json_t* object, const char* key) {
+//     std::vector<std::string> result;
+//     json_t* array = json_object_get(object, key);
+//     if (json_is_array(array)) {
+//         result.reserve(json_array_size((array)));
+//         size_t idx;
+//         json_t* element;
+//         json_array_foreach(object, idx, element) {
+//             auto content = json_string_value(element);
+//             result.emplace_back(std::string{content ? content : std::string()});
+//         }
+//     }
+//     return result;
+// }
+
+// std::string combine_jsonArrayStrings(const json_t* object, const char* key) {
+//     ostringstream result;
+//     result << "|";
+//     json_t* array = json_object_get(object, key);
+//     if (json_is_array(array)) {
+//         size_t idx;
+//         json_t* element;
+//         json_array_foreach(object, idx, element) {
+//             auto content = json_string_value(element);
+//             if (content) result << content << "|";
+//         }
+//     }
+//     return result.str();
+// }
+
+// std::vector<int> get_jsonArrayInts(const json_t* object, const char* key) {
+//     std::vector<int> result;
+//     json_t* array = json_object_get(object, key);
+//     if (json_is_array(array)) {
+//         result.reserve(json_array_size((array)));
+//         size_t idx;
+//         json_t* element;
+//         json_array_foreach(object, idx, element) {
+//             result.emplace_back(json_integer_value(element));
+//         }
+//     }
+//     return result;
+// }
 
 //replace all "search" with "replace" in "subject"
 string str_replace(const string& search, const string& replace, const string& subject) {
--- tools/stringhelpers.h
+++ tools/stringhelpers.h
@@ -3,12 +3,76 @@
 #define _STRINGHELPERS_H_
 
 #include <string>
+#include <vector>
+#include <iterator>
+#include <memory>
+#include <sstream>
+#include <iostream>
+
+#include <jansson.h>
+
+#include "../lib/common.h"
+
 using namespace std;
 
 string str_replace(const string& search, const string& replace, const string& subject);
 string str_cut(const string& search, const string& subject);
 
-#include <jansson.h>
+//
+// This is a wrapper for libjansson, intended to be used for read-only purposes.
+// the lifetime of the pointer to the json data structure is guaranteed by passing it as a shared_ptr to child nodes
+// 
+// To ease error handling for missing values (a lot of TMDB's fields are optional),
+// a default value is returned if no value is available ("" for a stringValue(), 0 for an json number).
+///
+
+class cJsonNode
+{
+   public:
+      cJsonNode() = default;
+      cJsonNode(const char* jsonString);
+      cJsonNode(const std::string &jsonString);
+      cJsonNode(json_t* data); // does not own/manage the given json_t*
+      cJsonNode(json_t* data, std::shared_ptr<json_t> root); // keeps root alive while used
+      ~cJsonNode() = default;
+      bool isNull(void);
+      bool isObject(void);
+      bool isInt(void);
+      bool isNumber(void);
+      bool isString(void);
+      bool isDouble(void);
+      bool isArray(void);
+      size_t currentArrayIndex(void);
+      size_t arraySize(void);
+      void resetArrayIterator(void); 
+      json_t* object(void);
+      json_t* objectByName(const char* name);
+      cJsonNode nodeByName(const char* name);
+      cJsonNode operator[](const size_t index);
+      cJsonNode operator[](const char* name);
+      cJsonNode operator[](const char* name) const;
+      cJsonNode operator[](const std::string &name);
+      cJsonNode operator[](const std::string &name) const;
+      cJsonNode nextElementInArray();
+      std::vector<cJsonNode> ArrayToVector(void);
+
+      cJsonNode getElementInArray(const size_t index);
+      int intByName(const char* name);
+      std::string stringByName(const char* name);
+      double doubleByName(const char* name);
+      double doubleValue();
+      int intValue(); // returns zero on error
+      std::string stringValue();
+      std::string combine_jsonArrayStrings(const char* sep="|");
+      void addStringArrayToVector(std::vector<std::string> &result);
+   protected:
+      json_t* obj = nullptr;
+      std::shared_ptr<json_t> root = nullptr; // this is the root of the json data structure
+                                              // if we create the json_t*, we need to keep this pointer alive until all objects accessing the
+                                              // json data have been destroyed - so we pack it into a shared_ptr with a destructor appropriate
+                                              // for a json_t* (see constructor implementation)
+      size_t idx = 0;
+};
 
 class cJsonLoader
 {
